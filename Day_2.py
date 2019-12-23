8f4498b4ca35b11f6684a94bb890bd2bbefb8363
Max <massimiliano.curzi@gmail.com>
2019-12-23 14:03:05 +0000

    update intcode python and day 2 implementation (python)

---
+++ Day_2.py
@@ -0,0 +1,30 @@
+''' Day 2 intcode '''
+from intcode import intcode
+from copy import deepcopy
+puzzle_input = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,6,23,1,23,6,27,1,13,27,31,2,13,31,35,1,5,35,39,2,39,13,43,1,10,43,47,2,13,47,51,1,6,51,55,2,55,13,59,1,59,10,63,1,63,10,67,2,10,67,71,1,6,71,75,1,10,75,79,1,79,9,83,2,83,6,87,2,87,9,91,1,5,91,95,1,6,95,99,1,99,9,103,2,10,103,107,1,107,6,111,2,9,111,115,1,5,115,119,1,10,119,123,1,2,123,127,1,127,6,0,99,2,14,0,0]
+
+# part 1
+# puzzle_input[1] = 12
+# puzzle_input[2] = 2
+
+computer = intcode.IntCode(program=deepcopy(puzzle_input))
+computer._mem[1] = 12
+computer._mem[2] = 2
+
+computer.run()
+print(f"Solution part 1: {computer._mem[0]}")
+
+# part2
+max_result = -1
+solution = -1
+for noun in range(100):
+    for verb in range(100):
+        #print(f"{noun=}, {verb=}")
+        computer = intcode.IntCode(program=deepcopy(puzzle_input))
+        computer._mem[1] = noun
+        computer._mem[2] = verb
+        computer.run()
+        if computer._mem[0] == 19690720:
+            solution = 100 * noun + verb
+
+print(f"Solution part 2: {solution}")
--- intcode/intcode.py
+++ intcode/intcode.py
@@ -14,39 +14,47 @@ class Mode(Enum):
 class Parameter:
     value: int
     mode: Mode
-    real_value: int
+    real_value: int = 0 # Initialise to 0, it will be assigned at runtime
 
 class IntCode(object):
-
-    def op_sum(self,a,b,out_addr):
+    
+    def op_sum(self, a, b, out_addr):
         self._mem[out_addr] = a+b
         return a+b
 
     def op_mul(self,a,b,out_addr):
         self._mem[out_addr] = a*b
+        return a+b
     
     def op_input(self,addr):
         self._mem[addr] = self._in_list.pop(0)
+        return self._mem[addr]
 
     def op_output(self,addr):
         self._out_list.append(self._mem[addr])
+        return self._mem[addr]
 
     def op_jumpiftrue(self,condition,addr):
         if condition:
             self._pc = addr
+        return condition, addr
 
     def op_jumpiffalse(self,condition,addr):
         if not condition:
             self._pc = addr
+        return condition, addr
 
     def op_lessthan(self,a,b,out_addr):
         self._mem[out_addr] = int(a<b)
+        return a,b,out_addr
     
     def op_equals(self,a,b,out_addr):
         self._mem[out_addr] = int(a==b)
+        return a,b,out_addr
 
     def op_addtorelbaseoffs(self, offset):
         self._pc_offset += offset
+        return self._pc_offset
     
     def op_terminate(self):
         self.done = True
@@ -59,21 +67,22 @@ class IntCode(object):
     _pc_offset = 0
     done = False
     operations = {} # Holds Params in/out
-    operations['1'] =  OpParams( 2, 1, op_sum )
-    operations['2'] =  OpParams( 2, 1, op_mul )
-    operations['3'] =  OpParams( 0, 1, op_input )
-    operations['4'] =  OpParams( 2, 1, op_output )
-    operations['5'] =  OpParams( 2, 0, op_jumpiftrue )
-    operations['6'] =  OpParams( 2, 0, op_jumpiffalse )
-    operations['7'] =  OpParams( 2, 1, op_lessthan )
-    operations['8'] =  OpParams( 2, 1, op_equals )
-    operations['9'] =  OpParams( 1, 0, op_addtorelbaseoffs )
+    operations['01'] =  OpParams( 2, 1, op_sum )
+    operations['02'] =  OpParams( 2, 1, op_mul )
+    operations['03'] =  OpParams( 0, 1, op_input )
+    operations['04'] =  OpParams( 2, 1, op_output )
+    operations['05'] =  OpParams( 2, 0, op_jumpiftrue )
+    operations['06'] =  OpParams( 2, 0, op_jumpiffalse )
+    operations['07'] =  OpParams( 2, 1, op_lessthan )
+    operations['08'] =  OpParams( 2, 1, op_equals )
+    operations['09'] =  OpParams( 1, 0, op_addtorelbaseoffs )
     operations['99'] = OpParams( 0, 0, op_terminate )
 
     def __init__(self, program, instance=0):
         assert(isinstance(program, list))
         self._mem = program
         self._pc = 0
+        _pc_offset = 0
         self._id = instance
 
     def push_input(self, v):
@@ -83,61 +92,72 @@ class IntCode(object):
         if isinstance(v, list):
             self._in_list.extend(v)
 
-    def step(self):
-        
+    def pop_output(self):
+        if len(self._out_list) > 0:
+            return self._out_list.pop(0)
+        else:
+            return None
+
+    def run(self):
         while self._pc < len(self._mem) and not self.done:
-            op_word = str(self._mem[self._pc]).rjust(5,'0')
+            self.step()
+
+    def step(self):
+        op_word = str(self._mem[self._pc]).rjust(5,'0')
+        #print(f"{op_word=}")
+        self._pc += 1
+        opcode = op_word[-2::]  # Last two characters are the opcode
+        
+        # # Check if we reached the end of the program
+        # if opcode=='99':
+        #     self.done
+        #     return
+
+        # Check if operation exists
+        assert opcode in self.operations, f"{opcode=}"
+
+        # Parameters with their mode
+        par_in = []
+        par_out = []
+        modes = list(op_word[0:3])
+
+        # Parse input parameters
+        for _ in range(self.operations[opcode].inparam):
+            par_in.append(Parameter(value = self._mem[self._pc],
+                                    mode = Mode(int(modes.pop(-1)))))
             self._pc += 1
-            opcode = op_word[-2::]  # Last two characters are the opcode
-            
-            # # Check if we reached the end of the program
-            # if opcode=='99':
-            #     self.done
-            #     return
 
-            # Check if operation exists
-            assert(opcode in self.operations)
+        # Parse output parameters
+        for _ in range(self.operations[opcode].outparam):
+            par_out.append(Parameter(value = self._mem[self._pc],
+                                    mode = Mode(int(modes.pop(-1)))))
+            self._pc += 1
+        
+        # Parse get values of parameters depending on their mode
+        for i in range(len(par_in)):
+            if par_in[i].mode == Mode.POSITION:
+                par_in[i].real_value = self._mem[par_in[i].value]
+            if par_in[i].mode == Mode.IMMEDIATE:
+                par_in[i].real_value = par_in[i].value
+            if par_in[i].mode == Mode.RELATIVE:
+                par_in[i].real_value = self._mem[par_in[i].value + self._pc_offset]
 
-            # Parameters with their mode
-            par_in = []
-            par_out = []
-            modes = list(op_word[0:3])
+        # Parse get values of parameters depending on their mode
+        for i in range(len(par_out)):
+            if par_out[i].mode == Mode.POSITION:
+                par_out[i].real_value = par_out[i].value
+            if par_out[i].mode == Mode.IMMEDIATE:
+                assert(0)
+            if par_out[i].mode == Mode.RELATIVE:
+                par_out[i].real_value = par_out[i].value + self._pc_offset
+        
+        pars = [p.real_value for p in par_in]
+        pars.extend([p.real_value for p in par_out])
 
-            # Parse input parameters
-            for _ in range(self.operations[opcode].inparam):
-                par_in.append(Parameter(value = self._mem[self._pc],
-                                        mode = Mode(int(modes.pop(-1)))))
-                self._pc += 1
+        # Execute operations
+        # print(f"Executing {self.operations[opcode].func} with {pars}")
 
-            # Parse output parameters
-            for _ in range(self.operations[opcode].outparam):
-                par_out.append(Parameter(value = self._mem[self._pc],
-                                        mode = Mode(int(modes.pop(-1)))))
-                self._pc += 1
-            
-            # Parse get values of parameters depending on their mode
-            for i in range(len(par_in)):
-                if par_in[i].mode == Mode.POSITION:
-                    par_in[i].real_value = self._mem[par_in[i].value]
-                if par_in[i].mode == Mode.IMMEDIATE:
-                    par_in[i].real_value = par_in[i].value
-                if par_in[i].mode == Mode.RELATIVE:
-                    par_in[i].real_value = self._mem[par_in[i].value + self._pc_offset]
-
-            # Parse get values of parameters depending on their mode
-            for i in range(len(par_out)):
-                if par_out[i].mode == Mode.POSITION:
-                    par_out[i].real_value = self._mem[par_out[i].value]
-                if par_out[i].mode == Mode.IMMEDIATE:
-                    par_out[i].real_value = par_out[i].value
-                if par_out[i].mode == Mode.RELATIVE:
-                    par_out[i].real_value = self._mem[par_out[i].value + self._pc_offset]
-            
-            pars = [p.real_value for p in par_in]
-            pars.extend([p.real_value for p in par_out])
-
-            # Execute operations
-            self.operations[opcode].func(*pars)
+        self.operations[opcode].func(self, *pars)
 
 
         
